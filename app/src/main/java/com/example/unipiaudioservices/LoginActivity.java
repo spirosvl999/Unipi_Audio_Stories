23fcd7149f57be88dd9469a31a30619e9d0d03ac
spirosvl999 <spirosvlachos.skr@outlook.com.gr>
2025-02-05 06:07:24 +0200

    Finished Log In and Register. Need to make the databases on firebase and also will finish all the work later on.

--- app/src/main/java/com/example/unipiaudioservices/LoginActivity.java
+++ app/src/main/java/com/example/unipiaudioservices/LoginActivity.java
@@ -3,12 +3,20 @@ package com.example.unipiaudioservices;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -16,12 +24,19 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.unipiaudioservices.databinding.ActivityLoginBinding;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
-
-
     TextView txtSignUp;
+    private FirebaseAuth mAuth;
+    EditText edtEmail, edtPassword;
+    Button btnSignIn;
+    String txtEmail, txtPassword;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,11 +45,64 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         txtSignUp = findViewById(R.id.txtSignUp);
 
+        edtEmail = findViewById(R.id.edtSignInEmail);
+        edtPassword = findViewById(R.id.edtSignInPassword);
+        progressBar = findViewById(R.id.signInProgressBar);
+        btnSignIn = findViewById(R.id.btnSignIn);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+
         txtSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent regIntent = new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(regIntent);
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtEmail = edtEmail.getText().toString().trim();
+                txtPassword = edtPassword.getText().toString().trim();
+
+                if (!TextUtils.isEmpty(txtEmail)) {
+                    if (txtEmail.matches(emailPattern)) {
+                        if (!TextUtils.isEmpty(txtPassword)) {
+                            SignInUser();
+                        } else {
+                            edtPassword.setError("Password Field can't be empty.");
+                        }
+                    } else {
+                        edtEmail.setError("Enter a valid Email Address.");
+                    }
+                } else {
+                    edtEmail.setError("Email Field can't be empty.");
+                }
+            }
+        });
+    }
+
+    private void SignInUser() {
+        progressBar.setVisibility(View.VISIBLE);
+        btnSignIn.setVisibility(View.INVISIBLE);
+
+        mAuth.signInWithEmailAndPassword(txtEmail, txtPassword).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(LoginActivity.this, "Login Successful !", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, "Error - " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.INVISIBLE);
+                btnSignIn.setVisibility(View.VISIBLE);
             }
         });
     }
--- app/src/main/java/com/example/unipiaudioservices/RegisterActivity.java
+++ app/src/main/java/com/example/unipiaudioservices/RegisterActivity.java
@@ -4,17 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     TextView txtSignIn;
+    EditText edtUserName, edtEmail,  edtPassword, edtConfirmPassword;
+    ProgressBar progressBar;
+    Button btnSignUp;
+    String txtUserName, txtEmail,  txtPassword, txtConfirmPassword;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +46,79 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
         txtSignIn = findViewById(R.id.txtSignIn);
+        edtUserName = findViewById(R.id.edtSignUpUsername);
+        edtEmail = findViewById(R.id.edtSignUpEmail);
+        edtPassword = findViewById(R.id.edtSignUpPassword);
+        edtConfirmPassword = findViewById(R.id.edtSignUpConfirmPassword);
+        progressBar = findViewById(R.id.signUpProgressBar);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         txtSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent logIntent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(logIntent);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                txtUserName = edtUserName.getText().toString();
+                txtEmail = edtEmail.getText().toString().trim();
+                txtPassword = edtPassword.getText().toString().trim();
+                txtConfirmPassword = edtConfirmPassword.getText().toString().trim();
+
+                if(!TextUtils.isEmpty(txtUserName)){
+                    if(!TextUtils.isEmpty(txtEmail)){
+                        if (txtEmail.matches(emailPattern)) {
+                            if(!TextUtils.isEmpty(txtPassword)){
+                                if(!TextUtils.isEmpty(txtConfirmPassword)){
+                                    if (txtConfirmPassword.equals(txtPassword)){
+                                        SingUpUser();
+                                    }else{
+                                        edtConfirmPassword.setError("Confirm Password Don't Match Password.");
+                                    }
+                                }else{
+                                    edtConfirmPassword.setError("Confirm Password Can't be empty.");
+                                }
+                            }else{
+                                edtPassword.setError("Password Can't be empty.");
+                            }
+                        }else{
+                            edtEmail.setError("Please enter a valid email.");
+                        }
+                    }else {
+                        edtEmail.setError("Email Can't be empty.");
+                    }
+                }else {
+                    edtUserName.setError("Username Can't be empty.");
+                }
+            }
+        });
+    }
+
+    private void SingUpUser() {
+        progressBar.setVisibility(View.VISIBLE);
+        btnSignUp.setVisibility(View.INVISIBLE);
+        mAuth.createUserWithEmailAndPassword(txtEmail, txtPassword).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(RegisterActivity.this, "Sign Up Successful !", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegisterActivity.this, "Error " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.INVISIBLE);
+                btnSignUp.setVisibility(View.VISIBLE);
             }
         });
     }
